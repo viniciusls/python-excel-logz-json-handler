03f74a92035c887c7710fb01caf188808bb7b2c0
Vinícius Silva <vinicius.ls@live.com>
2022-06-01 01:44:35 -0300

    add timestamp and remove ms on time to make it a number

--- main.py
+++ main.py
@@ -8,19 +8,24 @@ def main():
     json_str = csv_data.to_json(orient='records')
     json_array = json.loads(json_str)
 
+    requests_timestamp = []
     requests_status = []
     requests_time = []
     for json_obj in json_array:
-        message = json.loads(json_obj['message'])['message']
+        json_message = json.loads(json_obj['message'])
+        timestamp = json_message['time']
+        message = json_message['message']
         info = message.split(' | ')[0].split(' - ')
 
         if info[0] == 'Initializing Kevel configuration':
             continue
 
+        requests_timestamp.append(timestamp)
         requests_status.append(info[0])
-        requests_time.append(info[1])
+        requests_time.append(info[1].replace('ms', ''))
 
     info_dict = {
+        'timestamp': requests_timestamp,
         'status': requests_status,
         'time': requests_time
     }
